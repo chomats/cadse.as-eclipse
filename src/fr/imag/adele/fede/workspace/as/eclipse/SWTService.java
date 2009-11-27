c265b25bbe2aeb17a38c9ef4c0e6610d2bd83978
chomats <chomats@localhost>
2009-11-27 17:19:08 +0100

    (no commit message)

--- src/fr/imag/adele/fede/workspace/as/eclipse/SWTService.java
+++ src/fr/imag/adele/fede/workspace/as/eclipse/SWTService.java
@@ -5,17 +5,21 @@ import org.eclipse.ui.views.properties.IPropertySheetPage;
 
 import fr.imag.adele.cadse.core.CadseException;
 import fr.imag.adele.cadse.core.CadseRuntime;
+import fr.imag.adele.cadse.core.IItemNode;
 import fr.imag.adele.cadse.core.Item;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.LinkType;
+import org.eclipse.ui.part.IPageBookViewPage;
 
 public interface SWTService {
 
+	interface MyPropertySheetPage extends IPropertySheetPage, IPageBookViewPage {
+	}
 	public CadseRuntime[] openDialog(boolean askToErase);
 	
 	public boolean showCreateWizard(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
 	
 	public void showCreateWizardWithError(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
 
-	public IPropertySheetPage createPropertySheetPage();
+	public MyPropertySheetPage createPropertySheetPage(Object view, IItemNode node);
 }
