a8fcfb62766121db132341c70e014eeec2f8ad53
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-01-12 08:46:48 +0100

    move sources in main/java

---
+++ src/main/java/fr/imag/adele/fede/workspace/as/eclipse/SWTService.java
@@ -0,0 +1,25 @@
+package fr.imag.adele.fede.workspace.as.eclipse;
+
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.views.properties.IPropertySheetPage;
+
+import fr.imag.adele.cadse.core.CadseException;
+import fr.imag.adele.cadse.core.CadseRuntime;
+import fr.imag.adele.cadse.core.IItemNode;
+import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.ItemType;
+import fr.imag.adele.cadse.core.LinkType;
+import org.eclipse.ui.part.IPageBookViewPage;
+
+public interface SWTService {
+
+	interface MyPropertySheetPage extends IPropertySheetPage, IPageBookViewPage {
+	}
+	public CadseRuntime[] openDialog(boolean askToErase);
+	
+	public boolean showCreateWizard(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
+	
+	public void showCreateWizardWithError(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
+
+	public MyPropertySheetPage createPropertySheetPage(Object view, IItemNode node);
+}
