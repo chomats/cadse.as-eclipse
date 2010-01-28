7775be323b6cf81c99c28f03077877d90c5f6434
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-01-28 16:50:24 +0100

    implementation group progress fix cannot cancel wizard when has internal error fix is-root-element ?

--- src/main/java/fr/imag/adele/fede/workspace/as/eclipse/SWTService.java
+++ src/main/java/fr/imag/adele/fede/workspace/as/eclipse/SWTService.java
@@ -9,6 +9,8 @@ import fr.imag.adele.cadse.core.IItemNode;
 import fr.imag.adele.cadse.core.Item;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.LinkType;
+import fr.imag.adele.cadse.core.ui.view.NewContext;
+
 import org.eclipse.ui.part.IPageBookViewPage;
 
 public interface SWTService {
@@ -18,8 +20,10 @@ public interface SWTService {
 	public CadseRuntime[] openDialog(boolean askToErase);
 	
 	public boolean showCreateWizard(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
+	public boolean showCreateWizard(Shell parentShell, NewContext c) throws CadseException;
 	
 	public void showCreateWizardWithError(Shell parentShell, Item parent, LinkType lt, ItemType destItemType) throws CadseException;
+	public void showCreateWizardWithError(Shell parentShell, NewContext c) throws CadseException;
 
 	public MyPropertySheetPage createPropertySheetPage(Object view, IItemNode node);
 }
